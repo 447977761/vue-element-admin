a52e246bcfdb0ecf6453b673b863b5dbefcae148
liziyue <447977761@QQ.com>
2019-01-11 20:13:58 +0800

    教室预约功能楼层楼幢关系开发

--- src/views/bookingClassroom/index.vue
+++ src/views/bookingClassroom/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="table">
-    <el-tabs :tab-position="tabPosition">
+    <el-tabs :tab-position="tabPosition" @tab-click="changeBuildingTabs">
       <el-tab-pane label="全部">
         <el-table v-loading="loadingData" :data="tableData" border style="width: 100%" element-loading-text="加载中..." highlight-current-row>
           <el-table-column prop="classroomName" label="教室名称" align="center"/>
           <el-table-column prop="classroomCode" label="教室编码" align="center"/>
-          <el-table-column prop="isProjector" label="是否有投影仪" align="center"/>
-          <el-table-column prop="isComputer" label="是否有电脑" align="center"/>
-          <el-table-column prop="buildingCode" label="所属大楼" align="center"/>
-          <el-table-column prop="floor" label="所属楼层" align="center"/>
+          <el-table-column :formatter="isFlagFormatter" prop="isProjector" label="是否有投影仪" align="center"/>
+          <el-table-column :formatter="isFlagFormatter" prop="isComputer" label="是否有电脑" align="center"/>
+          <el-table-column prop="buildingName" label="所属大楼" align="center" min-width="130"/>
+          <el-table-column :formatter="floorFormatter" prop="floor" label="所属楼层" align="center"/>
           <el-table-column prop="oderCondition" label="预约情况" align="center"/>
           <el-table-column prop="classroomStatus" label="教室状态" align="center"/>
           <el-table-column prop="master" label="负责人" align="center"/>
@@ -38,16 +38,17 @@
             @current-change="pageCurrentChange"/>
         </div>
       </el-tab-pane>
-      <el-tab-pane label="经外楼">
-        <el-tabs :tab-position="chilfrentabPosition">
-          <el-tab-pane label="一楼">
-            <el-table v-loading="loadingData" :data="tableData" border style="width: 100%" element-loading-text="加载中..." highlight-current-row>
+      <el-tab-pane v-for="building in buildingInfos" :label="building.secondName" :key="building.buildingCode">
+        <el-tabs :tab-position="chilfrentabPosition" @tab-click="changeFloorTabs">
+          <el-tab-pane v-for="item in buildingEveryFloors" :label="item" :key="item">
+            <el-table v-loading="loadingData" :data="floorTableData" border style="width: 100%" element-loading-text="加载中..." highlight-current-row>
               <el-table-column prop="classroomName" label="教室名称" align="center"/>
               <el-table-column prop="classroomCode" label="教室编码" align="center"/>
-              <el-table-column prop="isProjector" label="是否有投影仪" align="center"/>
-              <el-table-column prop="isComputer" label="是否有电脑" align="center"/>
-              <el-table-column prop="floor" label="所属楼层" align="center"/>
+              <el-table-column :formatter="isFlagFormatter" prop="isProjector" label="是否有投影仪" align="center"/>
+              <el-table-column :formatter="isFlagFormatter" prop="isComputer" label="是否有电脑" align="center"/>
+              <el-table-column prop="buildingName" label="所属大楼" align="center" min-width="130"/>
               <el-table-column prop="oderCondition" label="预约情况" align="center"/>
+              <el-table-column prop="classroomStatus" label="教室状态" align="center"/>
               <el-table-column prop="master" label="负责人" align="center"/>
               <el-table-column prop="latestOder" label="最新预约情况" align="center"/>
               <el-table-column label="操作" width="150" align="center">
@@ -75,20 +76,14 @@
                 @current-change="pageCurrentChange"/>
             </div>
           </el-tab-pane>
-          <el-tab-pane label="二楼">二楼</el-tab-pane>
-          <el-tab-pane label="三楼">三楼</el-tab-pane>
-          <el-tab-pane label="四楼">四楼</el-tab-pane>
         </el-tabs>
       </el-tab-pane>
-      <el-tab-pane label="配置管理">配置管理</el-tab-pane>
-      <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-      <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
     </el-tabs>
   </div>
 </template>
 
 <script>
-
+import consts from '@/views/const/consts.js'
 export default {
 
   data() {
@@ -103,12 +98,20 @@ export default {
       total: 0,
       loadingData: false,
       tableData: [],
-      JWtableData: [],
-      getAllClassroomListUrl: '/shiep/crm/allClassroomList'
+      buildingInfos: [],
+      buildingEveryFloors: [],
+      floorTableData: [],
+      getAllClassroomListUrl: '/shiep/crm/allClassroomList',
+      getAllBuildingListUrl: '/shiep/crm/allBuildingList'
     }
   },
   created() {
+
+  },
+  mounted() {
     this.getAllClassroomList()
+    this.getAllBuildingList()
+    this.jdugeFloor(this.buildingInfos['floors'])
   },
   methods: {
     // 查询所有教室
@@ -127,6 +130,110 @@ export default {
         } else {
           if (res.data.message || res.data.msg) {
             self.tableData = []
+            self.$message.error(res.data.message || res.data.msg)
+          }
+        }
+      })
+    },
+    // 查询所有楼幢
+    getAllBuildingList() {
+      const self = this
+      self.loadingData = true
+      self.$axios.post(self.getAllBuildingListUrl).then((res) => {
+        self.loadingData = false
+        if (res.data.success) {
+          self.buildingInfos = res.data.result
+        } else {
+          if (res.data.message || res.data.msg) {
+            self.buildingInfos = []
+            self.$message.error(res.data.message || res.data.msg)
+          }
+        }
+      })
+    },
+    // 列处理
+    isFlagFormatter: function(row, column) {
+      var isProjector = row[column.property]
+      if (isProjector) {
+        var types = consts.isFlag.filter(function(v, i, arr) {
+          if (v.type === isProjector) {
+            return true
+          }
+        })
+        if (types[0]) {
+          return types[0].label
+        }
+        return isProjector
+      } else {
+        return ''
+      }
+    },
+    // 楼层列处理
+    floorFormatter: function(row, column) {
+      var floor = row[column.property]
+      if (floor) {
+        var types = consts.floors.filter(function(v, i, arr) {
+          if (v.type === floor) {
+            return true
+          }
+        })
+        if (types[0]) {
+          return types[0].label
+        }
+        return floor
+      } else {
+        return ''
+      }
+    },
+    jdugeFloor(item) {
+      console.log(item)
+      if (item) {
+        for (let j = 1; j <= item; j++) {
+          if (j) {
+            var types = consts.floors.filter(function(v, i, arr) {
+              if (v.type === j.toString()) {
+                return true
+              }
+            })
+            if (types[0]) {
+              this.buildingEveryFloors.push(types[0].label)
+            }
+          } else {
+            return ''
+          }
+        }
+      }
+    },
+    // 处理每幢楼
+    changeBuildingTabs: function(tab, event) {
+      this.buildingEveryFloors = []
+      for (let i = 0; i < this.buildingInfos.length; i++) {
+        if (event.target.getAttribute('id') === this.buildingInfos[i].tabId) {
+          this.jdugeFloor(this.buildingInfos[i].floors)
+        }
+      }
+    },
+    // 切换楼层获取数据
+    changeFloorTabs: function(tab, event) {
+      var str = event.target.getAttribute('id')
+      var strArr = str.split('-')
+      var res = (parseInt(strArr[1]) + 1).toString()
+      console.log(res)
+    },
+    // 获取对应楼下楼层教室  todo 做个表监视器用来监视哪些楼层可用
+    getBuildingClassroom(item) {
+      const self = this
+      const postDate = {
+        tabId: item
+      }
+      self.loadingData = true
+      self.$axios.post(self.getAllBuildingListUrl, postDate).then((res) => {
+        self.loadingData = false
+        if (res.data.success) {
+          self.buildingInfos = res.data.result
+        } else {
+          if (res.data.message || res.data.msg) {
+            self.buildingInfos = []
             self.$message.error(res.data.message || res.data.msg)
           }
         }
---
+++ src/views/const/consts.js
@@ -0,0 +1,63 @@
+const isFlag = [{
+  type: '1',
+  label: '是'
+}, {
+  type: '2',
+  label: '否'
+}]
+
+const floors = [{
+  type: '1',
+  label: '一楼'
+}, {
+  type: '2',
+  label: '二楼'
+}, {
+  type: '3',
+  label: '三楼'
+}, {
+  type: '4',
+  label: '四楼'
+}, {
+  type: '5',
+  label: '五楼'
+}, {
+  type: '6',
+  label: '六楼'
+}, {
+  type: '7',
+  label: '七楼'
+}, {
+  type: '8',
+  label: '八楼'
+}, {
+  type: '9',
+  label: '九楼'
+}, {
+  type: '10',
+  label: '十楼'
+}, {
+  type: '11',
+  label: '十一楼'
+}, {
+  type: '12',
+  label: '十二楼'
+}, {
+  type: '13',
+  label: '十三楼'
+}, {
+  type: '14',
+  label: '十四楼'
+}, {
+  type: '15',
+  label: '十五楼'
+}, {
+  type: '16',
+  label: '十六楼'
+}]
+
+export default
+{
+  isFlag,
+  floors
+}
